6ae430ddbcee9dc5a92c3ec12491cb590bac8baf
isuruch92 <isuruch92@gmail.com>
2024-10-20 14:35:41 +0300

    api

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ const server = jsonServer.create();
 // Uncomment to allow write operations
 const fs = require("fs");
 const path = require("path");
-const filePath = path.join("data/cities.json");
+const filePath = path.join("city-data.json");
 const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
 const router = jsonServer.router(db);
